c99621161e6f443ccb5ee07f70fedc92623021ed
kkania44 <kaniakrzysztof99@gmail.com>
2020-12-20 18:50:07 +0100

    define basic entities

---
+++ src/main/java/education/pl/planner/model/Subtopic.java
@@ -0,0 +1,28 @@
+package education.pl.planner.model;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@NoArgsConstructor
+@Entity
+@Table(name = "SUBTOPICS")
+public class Subtopic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne
+    @JoinColumn(name = "topic_id")
+    private Topic topic;
+    @NotEmpty
+    private String title;
+    private boolean completed = false;
+
+    public Subtopic(Topic topic, String title) {
+        this.topic = topic;
+        this.title = title;
+    }
+
+}
---
+++ src/main/java/education/pl/planner/model/Topic.java
@@ -0,0 +1,29 @@
+package education.pl.planner.model;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
+@Entity
+@Table(name = "TOPICS")
+public class Topic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotEmpty
+    private String title;
+    @OneToMany(mappedBy = "topic")
+    private Set<Subtopic> subtopics = new HashSet<>();
+    private int daysForLearning;
+    private boolean completed = false;
+
+    public Topic(String title, int daysForLearning) {
+        this.title = title;
+        this.daysForLearning = daysForLearning;
+    }
+}
