f6ab76637eea3891c7b215be405670d5ce6585a1
kkania44 <kaniakrzysztof99@gmail.com>
2020-12-28 21:11:48 +0100

    add cors config

---
+++ src/main/java/education/pl/planner/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package education.pl.planner.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods(HttpMethod.GET.name());
+    }
+}
